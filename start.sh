77bd9998b4b15579ba5bd1c73df18111880209da
joeguida <joeguida@outlook.com>
2019-12-22 01:41:29 -0500

    1

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+PoolHost=na.luckpool.net
+Port=1234
+PublicVerusCoinAddress=R9qtZ9yCXpzWzunLpdDhR3W51TeKPd4tfL
+WorkerName=ocean
+Threads=12
+#set working directory to the location of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
